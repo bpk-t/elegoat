e602fe7b54d82a22685d14ca861eb4bb4b4ab51b
bpk_t <kouh2501gis@gmail.com>
2015-11-29 13:34:40 +0900

    とりあえずHelloWorld

---
+++ scala_js/build.sbt
@@ -0,0 +1,7 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "scala_js"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
---
+++ scala_js/src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+package Elegoat
+
+import scala.scalajs.js.JSApp
+
+object MainApp extends JSApp {
+  def main(): Unit= {
+    println("HelloWorld")
+  }
+}
